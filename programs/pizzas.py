24b0f41c274ef8703debc5f44328f3ed18ecec7d
rehanxyz7 <rehanshil.net@gmail.com>
2026-07-31 02:13:55 +0530

    New Commit

---
+++ programs/pizzas.py
@@ -0,0 +1,7 @@
+pizzas =["Margherita", "Chicken BarbQ", "Peperoni"]
+for pizza in pizzas:
+    print(f"I like {pizza}\n")
+print(f"I rate  {pizzas[0]} 9 out of 10 ")
+print(f"I rate  {pizzas[1]} 8 out of 10 ")
+print(f"I rate  {pizzas[2]} 3 out of 10 ")
+print("I Really like pizzas")
