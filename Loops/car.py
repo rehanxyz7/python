cb8ff05c5014f0aeb2a4de2ce182e7e811b86d90
rehanxyz7 <rehanshil.net@gmail.com>
2026-08-11 01:04:28 +0530

    New COMMIT

---
+++ Loops/car.py
@@ -0,0 +1,2 @@
+car =input("Enter the car you would like to rent :")
+print(f"Let me see if there if I can find you a {car}! ")
---
+++ Loops/restaurant.py
@@ -0,0 +1,6 @@
+people = int(input("Enter the number of people for seating :"))
+if(people<8):
+    print("Your seats are ready sir ")
+    
+else:
+        print("You'll have to wait ")
