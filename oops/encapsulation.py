c9a4311821dff395702e838dad72d30682b46f01
rehanxyz7 <rehanshil.net@gmail.com>
2026-08-11 01:19:25 +0530

    New Commit

--- oops/encapsulation.py
+++ oops/encapsulation.py
@@ -3,11 +3,11 @@ class Car:
         Car.__brand = brand #making private and inaccessible directly to the users 
         Car.model = model
 
-    def get__brand(self):
+    def get__brand(self): #creating a get method to make private info accessible to user only if it is needed by them 
         return self.__brand 
     def fullname(self):
         return f"{self.__brand} {self.model}"
 
 my_car = Car("Toyota","SUPREME")
 print(my_car.fullname())
-print(my_car.get__brand())
+print(my_car.get__brand()) #to know the brand we need to actually call the get method to get the value
